dcf3a1ca7ae3ef21dc1cbe5bd4fe954666e1b31c
basmalamohamed938-hash <basmalamohamed938@gmail.com>
2026-04-10 19:26:08 +0200

    Create insert.py

---
+++ RBTree/insert.py
@@ -0,0 +1,18 @@
+from model import RBNode
+from .helpers import bst_insert
+from .fix_insert import fix_insert
+
+def insert(tree, value):
+    """Insert a new value into the Red-Black Tree."""
+    new_node = RBNode(value)
+    new_node.left = tree.NULL_LEAF
+    new_node.right = tree.NULL_LEAF
+    new_node.color = 'RED'
+
+    if tree.root is None:
+        tree.root = new_node
+        new_node.parent = None
+    else:
+        bst_insert(tree, tree.root, new_node)
+
+    fix_insert(tree, new_node)
